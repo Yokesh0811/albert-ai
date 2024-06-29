ebf59c393369923c7fbab0ac082b4d8dbef5f0e7
Yokesh0811 <yokeshrk08@gmail.com>
2024-06-29 15:15:38 +0530

    api usage limited

--- app/(dashboard)/(routes)/conversationPage/page.tsx
+++ app/(dashboard)/(routes)/conversationPage/page.tsx
@@ -2,17 +2,39 @@
 
 import { MessageSquare } from "lucide-react";
 import { Heading } from "../../../../components/heading";
-import Chat from "./Chat";
+import { Button } from "@/components/ui/button";
+import { useChat } from "ai/react";
 
 
 
-export default function convopage() {
+export default function chat() {
+
+    const { messages, input, handleInputChange, handleSubmit } = useChat();
+
     return (
         <div>
-            <header>
+            <header className="mt-4">
                 <Heading title="Conversation" description="Our Most advanced conversation model" icon={MessageSquare} iconColor="text-violet-500" bgColor="bg-violet-500/10" />
             </header>
-            <Chat />
+            <div className="h-full p-4 flex items-center justify-center">
+                <div className="reponseView">
+                    {messages.map((m) => (
+                        <div key={m.id} className="whitespace-pre-wrap">
+                            {m.role === 'user' ? 'User: ' : 'AI: '}
+                            {m.content}
+                        </div>
+                    ))}
+                </div>
+                <form onSubmit={handleSubmit} className="space-y-4 w-full h-full flex-col items-center justify-center">
+                    <input
+                        className="w-full px-3 py-4 border bg-transparent border-gray-300 rounded shadow-xl"
+                        value={input}
+                        placeholder="Say something..."
+                        onChange={handleInputChange}
+                    />
+                    <Button type="submit" className="flex-end">Generate</Button>
+                </form>
+            </div>
         </div>
 
     )
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,32 +1,42 @@
-import OpenAI from "openai";
-import { streamText, StreamingTextResponse } from "ai";
+// import OpenAI from "openai";
+// import { streamText, StreamingTextResponse } from "ai";
 
-export const maxDuration = 30;
+// export const maxDuration = 30;
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY || ' ',
-});
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY || ' ',
+// });
 
-export async function POST(req: Request, res: Response) {
+// export async function POST(req: Request, res: Response) {
+//     const { messages } = await req.json();
+//     console.log('messages:', messages)
+
+//     const response = await openai.chat.completions.create({
+//         model: "gpt-3.5-turbo",
+//         messages: [
+//             {
+//                 role: "system",
+//                 content: "You a called Albert one of the friendliest ai, you address people in a friendly way"
+//                     + "you often likes to crack jokes"
+//                     + "You go straight to the point, your replies are under 250 characters."
+//             },
+//             ...messages,
+//         ],
+//         temperature: 1,
+//     });
+
+//     const stream = await streamText(response);
+//     return new StreamingTextResponse(stream as unknown as ReadableStream);
+// }
+
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
+
+export async function POST(req: Request) {
     const { messages } = await req.json();
-    console.log('messages:', messages)
-
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                role: "system",
-                content: "You a called Albert one of the friendliest ai, you address people in a friendly way"
-                    + "you often likes to crack jokes"
-                    + "You go straight to the point, your replies are under 250 characters."
-            },
-            ...messages,
-        ],
-        temperature: 1,
+    const result = await streamText({
+        model: openai("gpt-4-1106-preview"),
+        messages,
     });
-
-    const stream = await streamText(response);
-    return new StreamingTextResponse(stream as unknown as ReadableStream);
+    return result.toAIStreamResponse();
 }
-
-
